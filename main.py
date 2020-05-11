5fd612ed4decfdca53b67356250b200676ac1d6f
rik <rikjbouwmeester@gmail.com>
2020-05-11 18:44:26 +0200

    multi-runway implementation

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# writes .lp file for optimization
-# idx 1 corresponds to flight 1 in csv, idx 0 corresponds to 'start', idx n to 'end'
-# todo; add multiple runways, add maximum delay
+# complete revision of linear programming model, more true to paper
+# largest change is removal of an optimal ordering system, this is not explicitly implemented in the paper, although
+# the author mentions that it is implicit in the objective function.
 
 import pandas as pd
+import numpy as np
 
 
-runway_list = ['l', 'r']
 flights_input = pd.read_csv('flights_input.csv')
 
 """ OBJECTIVE FUNCTION """
@@ -20,7 +20,7 @@ file.write('\n')
 file.write('Subject To\n')
 
 
-""" FUEL & DELAY """
+""" FUEL """
 # variables
 approach_left_distance = 10000      # m
 approach_right_distance = 20000     # m
@@ -31,6 +31,9 @@ mass_flow_heavy = 3
 velocity_approach_medium = 72       # m/s
 velocity_approach_heavy = 82        # m/s
 
+time_resolution = 10
+max_time_slot_shift = 500
+runway_list = ['l', 'r']
 
 total_fuel_burned_left_medium = mass_flow_medium * approach_left_distance / velocity_approach_medium
 total_fuel_burned_left_heavy = mass_flow_heavy * approach_left_distance / velocity_approach_heavy
@@ -48,117 +51,97 @@ for i in range(len(flights_input.index)):
         print('Error - aircraft class not found')
 file.write('= 0\n')
 
-for i in range(len(flights_input.index)):
-    file.write('  delay_maximum_' + str(i+1) + ': delay_maximum - delay_' + str(i+1) + ' >= 0\n')
+for lead_idx in range(len(flights_input.index)):
+    time_original = flights_input['Time'].iloc[lead_idx]
+    file.write('  delayerino_' + str(lead_idx) + ': ')
+    for j in range(max_time_slot_shift):
+        for runway in runway_list:
+            time_slot = time_original + j * time_resolution
+            file.write(' + ' + str(time_slot) + ' x_' + str(lead_idx + 1) + '_' + runway + '_' + str(time_slot))
+    file.write(' - delay_' + str(lead_idx+1) + ' = ' + str(time_original) + '\n')
 
-for i in range(len(flights_input.index)):
-    file.write('  time_maximum' + str(i+1) + ': time_maximum - time_' + str(i+1) + ' >= 0\n')
 
-
-""" SCHEDULING """
+""" FLIGHT ASSIGNMENT """
 # write
 for i in range(len(flights_input.index)):
-    arrival_time = flights_input['Time'].iloc[i]
-    file.write('  delay_' + str(i+1) + ': time_' + str(i+1) + ' - delay_' + str(i+1) + ' = ' + str(arrival_time) + '\n')
+    file.write('  assignment_' + str(i+1) + ': ')
+    time_original = flights_input['Time'] .iloc[i]
+    flag = True
+    for runway in runway_list:
+        for time_slot in range(time_original, time_original + max_time_slot_shift * time_resolution, time_resolution):
+            if flag:
+                file.write('x_' + str(i+1) + '_' + runway + '_' + str(time_slot))
+            else:
+                file.write(' + x_' + str(i + 1) + '_' + runway + '_' + str(time_slot))
+            flag = False
+    file.write(' = 1\n')
 
 
-""" SEPARATION """
-separation_heavy_heavy = -1687      # 113
-separation_heavy_medium = -1619     # 181
-separation_medium_heavy = -1713     # 87
-separation_medium_medium = -1700    # 100
+""" RUNWAY OCCUPATION """
+# dependency matrices
+separation_heavy_heavy = 113
+separation_heavy_medium = 181
+separation_medium_heavy = 87
+separation_medium_medium = 100
 
-for i in range(len(flights_input.index)):
-    for j in range(len(flights_input.index)):
-        if i is not j:
-            for k in runway_list:
-                file.write('  separation_' + str(i+1) + '_' + str(j+1) + '_' + k + ': time_' + str(j+1) + ' - time_' + str(i+1) +
-                           ' - 1800 order_' + str(i+1) + '_' + str(j+1) + '_' + k + ' >= ')
-                if flights_input['Weight class'] .iloc[i] == 'H':
-                    if flights_input['Weight class'] .iloc[j] == 'H':
-                        # heavy - heavy
-                        file.write(str(separation_heavy_heavy))
-                        file.write('\n')
-                    elif flights_input['Weight class'] .iloc[j] == 'M':
-                        # heavy - medium
-                        file.write(str(separation_heavy_medium))
-                        file.write('\n')
-                    else:
-                        print('Error - aircraft class not found')
-                elif flights_input['Weight class'] .iloc[i] == 'M':
-                    if flights_input['Weight class'].iloc[j] == 'H':
-                        # medium - heavy
-                        file.write(str(separation_medium_heavy))
-                        file.write('\n')
-                    elif flights_input['Weight class'] .iloc[j] == 'M':
-                        # medium - medium
-                        file.write(str(separation_medium_medium))
-                        file.write('\n')
-                    else:
-                        print('Error - aircraft class not found')
-                else:
-                    print('Error - aircraft class not found')
+dependency_tensor = []
+for lead_idx in range(len(flights_input.index)):
+    lead_class = flights_input['Weight class'] .iloc[lead_idx]
+    lead_time_original = flights_input['Time'].iloc[lead_idx]
+    lead_matrix = []
+    for follow_idx in range(len(flights_input.index)):      # todo; to optimize performance, section 4.4; i-10,i+30
+        dependency_matrix = np.zeros((max_time_slot_shift, max_time_slot_shift))
+        if follow_idx is not lead_idx:
+            follow_class = flights_input['Weight class'].iloc[follow_idx]
+            follow_time_original = flights_input['Time'].iloc[follow_idx]
+            for i in range(max_time_slot_shift):
+                for j in range(max_time_slot_shift):
+                    dependency = False
+                    lead_time_slot = flights_input['Time'].iloc[lead_idx] + i * time_resolution
+                    follow_time_slot = flights_input['Time'].iloc[follow_idx] + j * time_resolution
+                    if follow_time_slot >= lead_time_slot: # todo; check if follower HAS to be after leader
+                        separation = follow_time_slot - lead_time_slot
+                        if lead_class == 'H':
+                            if follow_class == 'H':
+                                if separation < separation_heavy_heavy:
+                                    dependency = True
+                            elif follow_class == 'M':
+                                if separation < separation_heavy_medium:
+                                    dependency = True
+                        elif lead_class == 'M':
+                            if follow_class == 'H':
+                                if separation < separation_medium_heavy:
+                                    dependency = True
+                            elif follow_class == 'M':
+                                if separation < separation_medium_medium:
+                                    dependency = True
+                    dependency_matrix[i][j] = dependency
+        lead_matrix.append(dependency_matrix)
+    dependency_tensor.append(lead_matrix)
 
-
-""" ORDER """
-# ascertain that from order i_j and j_i, maximum one can be True.
-for i in range(len(flights_input.index)):
-    for j in range(len(flights_input.index)):
-        if i < j:
-            file.write('  o_' + str(i + 1) + '_' + str(j + 1) + ':')
-            flag = True
-            print(i,j)
-            for k in runway_list:
-                if flag:
-                    file.write(' order_' + str(j+1) + '_' + str(i+1) + '_' + k + ' + order_' + str(i+1) + '_' + str(j+1) + '_' + k)
-                if not flag:
-                    file.write(' + order_' + str(j + 1) + '_' + str(i + 1) + '_' + k + ' + order_' + str(i + 1) + '_' + str(j + 1) + '_' + k)
-                flag = False
+# write
+for runway in runway_list:
+    for lead_idx in range(len(flights_input.index)):
+        time_original = flights_input['Time'].iloc[lead_idx]
+        for j in range(max_time_slot_shift):
+            time_slot = time_original + j * time_resolution
+            file.write('  occupation_' + str(lead_idx + 1) + '_' + str(time_slot) + '_' + runway + ': x_' + str(lead_idx + 1) + '_' + runway + '_' + str(time_slot))
+            for follow_idx in range(len(flights_input.index)):
+                if follow_idx is not lead_idx:
+                    for k in range(max_time_slot_shift):
+                        if dependency_tensor[lead_idx][follow_idx][j][k]:
+                            follow_time_slot = flights_input['Time'].iloc[follow_idx] + k * time_resolution
+                            # if follow_time_slot >= time_slot:
+                            file.write(' + x_' + str(follow_idx+1) + '_' + runway + '_' + str(follow_time_slot))
             file.write(' <= 1\n')
-# ascertain that from order i_n for n = 1, 2, 3 ... but not i; maximum one can be true
-# this ensures that only one flight can be directly AFTER flight i
-for i in range(len(flights_input.index)+1):
-    file.write('  after' + str(i) + ':')
-    flag = True
-    for j in range(1, len(flights_input.index)+2):
-        if i is not j:
-            for k in runway_list:
-                if flag:
-                    file.write(' order_' + str(i) + '_' + str(j) + '_' + k)
-                if not flag:
-                    file.write(' + order_' + str(i) + '_' + str(j) + '_' + k)
-                flag = False
-    file.write(' = 1\n')
-
-# ascertain that from order n_i for n = 1, 2, 3 ... but not i; maximum one can be true
-# this ensures that only one flight can be directly BEFORE flight i
-for i in range(1, len(flights_input.index)+2):
-    file.write('  before' + str(i) + ':')
-    flag = True
-    for j in range(len(flights_input.index)+1):
-        if i is not j:
-            for k in runway_list:
-                if flag:
-                    file.write(' order_' + str(j) + '_' + str(i) + '_' + k)
-                if not flag:
-                    file.write(' + order_' + str(j) + '_' + str(i) + '_' + k)
-                flag = False
-    file.write(' = 1\n')
-
-
-""" NOISE """
-# variables
-noise_left_medium = 65      # dB
-noise_left_heavy = 70       # dB
-noise_right_medium = 70     # dB
-noise_right_heavy = 75      # dB
 
 
 """ DEFINE VARIABLES """
 file.write('\nBinary\n')
-for i in range(len(flights_input.index)+2):
-    for j in range(len(flights_input.index)+1):
-        if i is not j:
-            for k in runway_list:
-                file.write('order_' + str(j) + '_' + str(i) + '_' + k + ' ')
+for runway in runway_list:
+    for lead_idx in range(len(flights_input.index)):
+        time_original = flights_input['Time'].iloc[lead_idx]
+        for j in range(max_time_slot_shift):
+            time_slot = time_original + j * time_resolution
+            file.write('x_' + str(lead_idx + 1) + '_' + runway + '_' + str(time_slot) + ' ')
 file.write('\nEnd')
